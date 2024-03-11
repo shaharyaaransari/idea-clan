1a3c28fdb51848490549fe524aecd9e211b0315f
shaharyaaransari <shaharyaaransari786@gmail.com>
2024-03-11 20:20:21 +0530

    backend initailize

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { mongoose } = require("mongoose");
+
+const app = express();
+   const cors = require("cors")
+require("dotenv").config()
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+        res.send("welcome to HomePage")
+})
+
+const connections = async () => {
+        try {
+                await mongoose.connect(process.env.MONGO_URL)
+                console.log("connected!..")
+        } catch (error) {
+                console.log(error)
+        }
+}
+
+app.listen(8080, () => {
+        connections()
+        console.log("server runnig..")
+})
+
+
